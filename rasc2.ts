1a82e2a8a36b0543fe8256baec6ebc7b17ae0561
Gustavonvp <gustavonvp@gmail.com>
2021-12-20 19:16:15 -0300

    Using class with types and constructors

---
+++ rasc2.ts
@@ -0,0 +1,23 @@
+interface Driveable {
+    speed: number;
+    drive(): string;
+}
+
+
+
+class Car implements Driveable {
+    speed = 10;
+
+    drive() {
+        return `Iam driving at ${this.speed}`;
+    }
+}
+
+const myCarx = new Car();
+const startDriving  = (vehicle: Driveable) => {
+    vehicle.drive();
+};
+
+
+// startDriving(myCar);
+
