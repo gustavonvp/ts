c40a888caec42fda46c3eb747cad5dd447e59b5f
Gustavonvp <gustavonvp@gmail.com>
2021-12-21 16:53:39 -0300

    tap cat event operation RxOperators

--- annotations_lowlevel_subscriber.js
+++ annotations_lowlevel_subscriber.js
@@ -1,23 +1,25 @@
 const { Observable } = Rx;
+const { tap } = RxOperators;
 
 const observable = new Observable((subscriber) => {
    // Throw the value 1 into our pipeline
- 		subscriber.next(1);
+ 	subscriber.next(1);
+  subscriber.next(2);
+  subscriber.next(3);
   
   subscriber.error(new Error('terapia'));
   
   // Marks the observable as complete, no more values will come out
   subscriber.complete();
 
-  
-  
  
-});
+}).pipe(tap(value => console.log('From tap:', value))
+);
 
 observable.subscribe(
     (value) => console.log('Next value:', value),
     (err) => console.log('BaD THING!!!!', err.message),
-    () => console.log('COMPLETE'),
+    () => console.log('COMPLETE') //completition
 );
 
 observable;
