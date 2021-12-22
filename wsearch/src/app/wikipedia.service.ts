efe0c7aaeb92dd9de624002ec44fc150084185ca
Gustavonvp <gustavonvp@gmail.com>
2021-12-21 23:20:49 -0300

    Using pluck to get propertiees on functions

--- wsearch/src/app/wikipedia.service.ts
+++ wsearch/src/app/wikipedia.service.ts
@@ -1,5 +1,37 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { pluck } from 'rxjs/operators';
+
+
+
+interface Car {
+  year: number;
+  color: string;
+  running: boolean;
+  make: {
+    name: string;
+    dateCreated: number;
+  }
+}
+
+const observable = new Observable<Car>(observer => {
+  observer.next({
+    year: 2000,
+    color: 'red',
+    running: true,
+    make: {
+      name: 'Chevy',
+      dateCreated: 1950
+    }
+  });
+}).pipe(
+    pluck('make', 'dateCreated')
+);
+observable.subscribe(value => {
+  console.log(value);
+});
+
 
 @Injectable({
   providedIn: 'root'
